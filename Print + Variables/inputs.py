88019549a9e451901bf4f8fed089717a37499ac1
RyanMcCarthy149 <114451189+RyanMcCarthy149@users.noreply.github.com>
2026-01-22 21:53:31 +0000

    Create inputs.py

---
+++ Print + Variables/inputs.py
@@ -0,0 +1,8 @@
+#Prompt user for input
+input("What are the numbers Mason? ")
+
+#Prompt User and display answer
+num = input("What are the numbers Mason? ")
+print(num)
+
+#13 minutes https://www.youtube.com/watch?v=XWuP5Yf5ILI
