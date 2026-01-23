48255bc167480b22b4e2e3f0c620264d6483c904
RyanMcCarthy149 <114451189+RyanMcCarthy149@users.noreply.github.com>
2026-01-23 23:24:47 +0000

    Update inputs.py

--- Print + Variables/inputs.py
+++ Print + Variables/inputs.py
@@ -3,6 +3,7 @@ input("What are the numbers Mason? ")
 
 #Prompt User and display answer
 num = input("What are the numbers Mason? ")
-print(num)
+print("The Numbers are" + num)
 
-#13 minutes https://www.youtube.com/watch?v=XWuP5Yf5ILI
+# Do this all in one line
+print("The Numbers are " + input("What are the number Mason? "))
