fc0bb6c0868c875de9c7d5303cc8a36fda06cfef
RyanMcCarthy149 <114451189+RyanMcCarthy149@users.noreply.github.com>
2026-01-23 23:47:19 +0000

    Create challenge1.py

---
+++ Variables Practice/challenge1.py
@@ -0,0 +1,6 @@
+# Take a first name as a variable and last name, then print to the console "Your name is _____"
+
+fname = input("What is your first name? ")
+lname = input("What is your last name? ")
+
+print(f"Your name is {fname} {lname}")
